9d88bc680bce20d2fc7fd28a733ef16148e1b540
gunadevandesing <gunadevandesing@gmail.com>
2024-06-13 16:57:19 +0000

    App update

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
       <h1>Vite + React App 1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -22,9 +17,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
