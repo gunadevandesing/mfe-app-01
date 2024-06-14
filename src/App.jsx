3f1c20a14683f261db0499e6afbef8a91dd5a298
gunadevandesing <gunadevandesing@gmail.com>
2024-06-14 10:23:17 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           GD - {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
     </>
   )
